7c090d3d96b77a7ebcdd5d1ca79dfcb42e26ade5
Ikaro <ikaroalef@gmail.com>
2016-06-09 18:17:02 -0300

    knapsack

--- Problema da Mochila/src/beans/Mochila.java
+++ Problema da Mochila/src/beans/Mochila.java
@@ -36,7 +36,22 @@ public class Mochila {
 	public void removeUltimoObj(){
 		objetos.remove(objetos.size()-1);
 	}
-	
+	/*
+	public double knapsack(ArrayList<Objeto>itens, double C){
+
+		int n = itens.size();
+		itens[2][N + 1] = itens [1][N] ;
+		itens 0 ← NIL;
+		criar(maxTab[N+1][C+1]); //Inicialize com 0 toda a linha 0 e também a coluna 0
+		para i ← 1 até N faça
+		para j ← 1 até C faça
+		atual
+		if (itens[i]. peso ≤ j)  // se o item i cabe na mochila
+			maxTab[i][j] ← max(maxTab[i − 1][j];
+		else maxTab[i][j] ← maxTab[i − 1][j];
+		return maxTab[N][C]
+	}
+	*/
 	public static void main(String[] args) {
 		Mochila mochila = new Mochila(100);
 		ArrayList<Objeto> objetos = new ArrayList<Objeto>(Arrays.asList( //array com obejtos a serem inseridos
@@ -51,10 +66,11 @@ public class Mochila {
 				new Objeto(11,15),
 				new Objeto(5,7)));
 		int i = 0;
+		/*
 		while (mochila.getPesoAtual() < mochila.getPesoMax()){
 			mochila.insereObj(objetos.get(i));
 			i++;
 		}
-		
+		*/
 	}
 }
