e5da32f8dca7fb16545edb1ff20d732d3004c5f3
Ikaro <ikaroalef@gmail.com>
2016-06-14 15:26:44 -0300

    Ajustes finos

--- Problema da Mochila/src/beans/Knapsack.java
+++ Problema da Mochila/src/beans/Knapsack.java
@@ -2,58 +2,48 @@ package beans;
 
 class Knapsack {
     public static void main(String[] args) throws Exception {
-        int val[] = {10, 40, 30, 50, 10, 20};//vetor de valores
-        int wt[] = {1, 4, 6, 3, 1, 5};//vetor de pesos
-        int W = 10;//capacidade da mochila
-        System.out.println(knapsack(val, wt, W)); //imprime valor total que compõe a mochila
+        int valores[] = {10, 40, 30, 50, 10, 20};//vetor de valorores
+        int pesos[] = {1, 4, 6, 3, 1, 5};//vetor de pesos
+        int pesoMax = 10;//capacidade da mochila
+        System.out.println(knapsack(valores, pesos, pesoMax)); //imprime valoror total que compõe a mochila
     }
 
-    public static int knapsack(int val[], int wt[], int W) {
+    public static int knapsack(int valores[], int pesos[], int pesoMax) {
 
         //N representa o numero total de itens dispostos
-        int N = wt.length; 
+        int N = pesos.length; 
 
         //criar matriz
-        int[][] V = new int[N + 1][W + 1]; 
+        int[][] V = new int[N + 1][pesoMax + 1]; 
 
-        //Se a capacidade da mochila é 0, defina todas as colunas da linha 0 para o valor 
-        for (int col = 0; col <= W; col++) {
-            V[0][col] = 0;
+        //Se a capacidade da mochila é 0, defina todas as colunas da linha 0 para o valoror 
+        for (int coluna = 0; coluna <= pesoMax; coluna++) {
+            V[0][coluna] = 0;
         }
         //Se não há itens na casa - What if there are no items at home. (supoe q um ladrão vai roubar uma casa, porém não há itens para serem roubados)  
-        //Preencha a primeira linha com 0 - Fill the first row with 0
+        //Preencha a primeira linha com 0
         for (int row = 0; row <= N; row++) {
             V[row][0] = 0;
         }
         for (int item=1;item<=N;item++){
-            //Vamos preencher os valores linha a linha - Let's fill the values row by row
-            for (int weight=1;weight<=W;weight++){
-                //Se o peso do item atual é menor ou igual ao peso da mochila atual 
-            	//Is the current items weight less than or equal to running weight
-                if (wt[item-1]<=weight){
-//Dado um peso, checa se o valor do item atual somado com o valor do item que poderíamos dispor com o peso restante é maior que o valor sem o item atual  
-//Given a weight, check if the value of the current 
-//item + value of the item that we could afford 
-//with the remaining weight is greater than the value
-//without the current item itself
-                    V[item][weight]=Math.max (val[item-1]+V[item-1][weight-wt[item-1]], V[item-1][weight]);
-                }
-                else {
-//Se o peso do item atual é maior que o peso em execução, apenas carregue o valor sem o item atual.                	
-//If the current item's weight is more than the
-//running weight, just carry forward the value
-//without the current item
-                    V[item][weight]=V[item-1][weight];
-                }
+            //Vamos preencher os valorores linha a linha
+            for (int peso=1;peso<=pesoMax;peso++){
+                //Se o pesos do item atual é menor ou igual ao pesos da mochila atual 
+                if (pesos[item-1]<=peso)
+//Dado um pesos, checa se o valoror do item atual somado com o valoror do item que poderíamos dispor com o pesos restante é maior que o valoror sem o item atual  
+                    V[item][peso]=Math.max (valores[item-1]+V[item-1][peso-pesos[item-1]], V[item-1][peso]);
+                else
+//Se o pesos do item atual é maior que o pesos em execução, apenas carregue o valoror sem o item atual.
+                    V[item][peso]=V[item-1][peso];
             }
         }
         //imprimir a matriz 
-        for (int[] rows : V) {
-            for (int col : rows) {
-                System.out.format("%5d", col);
+        for (int[] linha : V) {
+            for (int coluna : linha) {
+                System.out.format("%5d", coluna);
             }
             System.out.println();
         }
-        return V[N][W];
+        return V[N][pesoMax];
     }
 }
